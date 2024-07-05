f4f525cb3453405abff2fefc84d6d74cabdc1db9
Nihal Baranwal <nihalkumar9749@gmail.com>
2024-07-05 13:20:48 +0530

    Navbar Design

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./navbar.css";
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -13,38 +14,34 @@ const Navbar = () => {
     }
 
     return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="nav-parent">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark my-navbar">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <a className="navbar-brand nav-logo" href="#">MintSpace</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className="navbar-items collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item" onClick={() => navigate("/")}>
                                 <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">My NFTs</a>
+                                <a className="nav-link active" href="#">My NFTs</a>
                             </li>
                             <li className="nav-item" onClick={() => redirectSeller()}>
-                                <a className="nav-link" href="#">Sell NFTs</a>
+                                <a className="nav-link active" href="#">Sell NFTs</a>
                             </li>
                             <li className="nav-item" onClick={() => redirectDashboard()}>
-                                <a className="nav-link" href="#">Creator Dashboard</a>
+                                <a className="nav-link active" href="#">Creator Dashboard</a>
                             </li>
 
 
                         </ul>
-                        <form className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
                     </div>
                 </div>
             </nav>
-        </>
+        </div>
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../components/navbar';
 import Card from '../components/card';
+import './Home.css';
 
 const Home = () => {
 
